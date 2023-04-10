3f7250d867a49fd74b4b189841841b911ab19ee0
Vanshika-Saini <74698779+Vanshika-Saini@users.noreply.github.com>
2023-04-10 17:35:37 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,205 @@
+//sidebar
+const menuItems = document.querySelectorAll(".menu-item");
+//message
+const messagesNotification = document.querySelector("#messages-notification");
+const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message");
+const messageSearch = document.querySelector("#message-search");
+
+//Theme
+const theme = document.querySelector("#theme");
+const themeModal = document.querySelector(".customize-theme");
+const fontSizes = document.querySelectorAll(".choose-size span");
+var root = document.querySelector(":root");
+const colorPalette = document.querySelectorAll(".choose-color span");
+const bg1 = document.querySelector(".bg-1");
+const bg2 = document.querySelector(".bg-2");
+const bg3 = document.querySelector(".bg-3");
+
+
+//=================== SIDEBAR ======================//
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove("active");
+    });
+}
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        changeActiveItem();
+        item.classList.add("active");
+        if(item.id != "notifications"){
+            document.querySelector(".notifications-popup").style.display = "none";
+        }else{
+            document.querySelector(".notifications-popup").style.display = "block";
+            document.querySelector("#notifications .notification-count").style.display = "none";
+        }
+    })
+});
+
+
+//=================== Messages ======================//
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelector("h5").textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = "flex";
+        }else{
+            chat.style.display = "none";
+        }
+    });
+}
+
+
+messageSearch.addEventListener("keyup", searchMessage);
+
+
+
+messagesNotification.addEventListener("click", () => {
+    messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+    messagesNotification.querySelector(".notification-count").style.display = "none";
+    setTimeout(() => {
+        messages.style.boxShadow = "none";
+    }, 2000)
+});
+
+//theme display customization
+
+const openThemeModal = () =>{
+    themeModal.style.display = "grid";
+}
+
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains("customize-theme")){
+        
+        themeModal.style.display = "none";
+    }
+}
+
+themeModal.addEventListener("click", closeThemeModal);
+
+theme.addEventListener("click", openThemeModal);
+
+//fonts
+const removeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove("active");
+    });
+}
+
+fontSizes.forEach(size => {
+   size.addEventListener("click", () => {
+        removeSelector();
+        let fontSize;
+        size.classList.toggle("active");
+
+        if(size.classList.contains("font-size-1")){
+            fontSize = "10px";
+            root.style.setProperty(" --sticky-top-left", "5.4rem");
+            root.style.setProperty(" --sticky-top-right", "5.4rem");
+        }else if(size.classList.contains("font-size-2")){
+            fontSize = "14px";
+            root.style.setProperty(" --sticky-top-left", "5.4rem");
+            root.style.setProperty(" --sticky-top-right", "-7rem");
+        }else if(size.classList.contains("font-size-3")){
+            fontSize = "16px";
+            root.style.setProperty(" --sticky-top-left", "-2rem");
+            root.style.setProperty(" --sticky-top-right", "-17rem");
+        }else if(size.classList.contains("font-size-4")){
+            fontSize = "19px";
+            root.style.setProperty(" --sticky-top-left", "-5rem");
+            root.style.setProperty(" --sticky-top-right", "-25rem");
+        }else if(size.classList.contains("font-size-5")){
+            fontSize = "22px";
+            root.style.setProperty(" --sticky-top-left", "-10rem");
+            root.style.setProperty(" --sticky-top-right", "-33rem");
+        }
+
+    document.querySelector("html").style.fontSize = fontSize;
+   });
+
+
+    
+});
+
+
+//change primary colors
+
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove("active");
+    })
+}
+
+colorPalette.forEach(color => {
+    color.addEventListener("click", () => {
+        let primary;
+        changeActiveColorClass();
+
+        if(color.classList.contains("color-1")){
+            primaryHue = 252;
+        }else if(color.classList.contains("color-2")){
+            primaryHue = 52;
+        }else if(color.classList.contains("color-3")){
+            primaryHue = 352;
+        }else if(color.classList.contains("color-4")){
+            primaryHue = 152;
+        }else if(color.classList.contains("color-5")){
+            primaryHue = 202;
+        }
+        color.classList.add("active");
+
+        root.style.setProperty("--primary-color-hue", primaryHue);
+        
+    })
+});
+
+
+//theme background
+let lightColorLighetness;
+let whiteColorLighetness;
+let darkColorLighetness;
+
+const changeBg = () => {
+    root.style.setProperty("--light-color-lighetness", lightColorLighetness);
+    root.style.setProperty("--white-color-lighetness", whiteColorLighetness);
+    root.style.setProperty("--dark-color-lighetness", darkColorLighetness);
+}
+
+bg1.addEventListener("click", () => {
+
+    bg1.classList.add("active");
+
+    bg3.classList.remove("active");
+    bg2.classList.remove("active");
+
+    window.location.reload();
+});
+
+bg2.addEventListener("click", () => {
+    darkColorLighetness = "95%";
+    whiteColorLighetness = "20%";
+    lightColorLighetness = "15%";
+
+    bg2.classList.add("active");
+
+    bg1.classList.remove("active");
+    bg3.classList.remove("active");
+
+    changeBg();
+});
+
+bg3.addEventListener("click", () => {
+    darkColorLighetness = "95%";
+    whiteColorLighetness = "10%";
+    lightColorLighetness = "0%";
+
+    bg3.classList.add("active");
+
+    bg1.classList.remove("active");
+    bg2.classList.remove("active");
+
+    changeBg();
+});
+
