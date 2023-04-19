caa84ffa3bf06a58f49de9ef26b037aeea1e83b0
Maksim Kantur <maxim.kantur@hotmail.com>
2023-04-19 23:28:20 +0300

    simplify test2

--- src/middleware/picture.middleware.js
+++ src/middleware/picture.middleware.js
@@ -6,26 +6,24 @@ export function sharePicture(pictureInfo) {
     formData.append('userId', pictureInfo.userId);
     formData.append('description', pictureInfo.description);
     formData.append('file', pictureInfo.pictureImage);
-  
+
     console.log(formData);
     return fetch(CURRENT_SERVER_API + '/picture', {
-      method: 'POST',
-      headers: { 'Accept': 'application/json' },
-      body: formData
+        method: 'POST',
+        headers: { 'Accept': 'application/json' },
+        body: formData
     })
-      .then(res => {
-        if (res.ok) return res.json();
-      })
-      .catch(err => {
-        console.log('error');
-        console.log(err);
-        throw new Error('Sharing picture failed');
-      })
-      .then(resData => {
-        console.log('res data');
-        console.log(resData);
-      });
-  }
+        .then(res => {
+            if (res.ok) return res.json();
+        }).catch(err => {
+            console.log('error');
+            console.log(err);
+            throw new Error('Sharing picture failed');
+        }).then(resData => {
+            console.log('res data');
+            console.log(resData);
+        })
+}
 
 export function updatePictureCaption(pictureId, caption) {
     return fetch(CURRENT_SERVER_API + '/picture/updatePictureCaption', {
